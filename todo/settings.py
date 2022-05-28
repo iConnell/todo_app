eff71529ab8c47ad7c38ac7d92f18825bb08bc27
McConnell <ik.ugwuanyi@gmail.com>
2022-05-28 19:57:52 +0100

    removed session authentication

--- todo/settings.py
+++ todo/settings.py
@@ -113,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
